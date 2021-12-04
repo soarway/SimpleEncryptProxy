59cbf7c523d879052c612f3233684218aba94539
soarway <43486326+soarway@users.noreply.github.com>
2021-12-04 12:17:39 +0800

    Add files via upload
    简单的加密代理程序，client_proxy加密， server_proxy解密后推送给最终的server，  server_proxy收到数据后加密, 然后发送给client_proxy，client_proxy解密后发给终端,
    加密算法就是简单的异或加密。

---
+++ winproxy/ssl/ssl.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+
+#include <stdio.h>
+
+#define MAX_DATA  8192
+
+
+
+
+int main()
+{
+    char a[] = "1中国-人名大学fa大福大";        //要加密的密码
+    char b[] = "A";     //密钥
+    int len = strlen(a);
+    int i;
+    //加密代码
+    for (i = 0; i < len; i++)
+    {
+        a[i] = ~a[i];
+        a[i] = a[i] ^ b[0];
+    }
+    printf("Your Password is encrypted: %s\n", a);
+
+    /*解密代码*/
+    for (i = 0;i < len;i++)
+    {
+        a[i] = a[i] ^ b[0];
+        a[i] = ~a[i];
+    }
+
+    printf("You Password: %s\n", a);
+
+    return 0;
+}
+
